ff20940e1e298e56bdf85ba459a2fb6a5994da41
CyrilGrj <cyril.grosjean@outlook.fr>
2023-10-12 20:35:59 +0200

    requirement.txt

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 
 
+
 #Import personnalisé
-import model
+#import model
 
 
 load_dotenv()
@@ -16,7 +17,7 @@ app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = 'votre_clé_secrète'  # Remplacez 'votre_clé_secrète' par une clé secrète forte et sécurisée
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
-#db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 jwt = JWTManager(app)
 
 @app.route('/login', methods=['POST'])
@@ -38,5 +39,19 @@ def ressource_protégée():
     current_user = get_jwt_identity()
     return {'message': 'Ceci est une ressource protégée', 'user': current_user}
 
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+with app.app_context():
+    db.create_all()
+    
 if __name__ == '__main__':
     app.run(debug=True)
