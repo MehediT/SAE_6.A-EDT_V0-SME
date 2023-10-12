01e664f2eb15985a0a8615984a4d46db17147f2e
CyrilGrj <cyril.grosjean@outlook.fr>
2023-10-12 15:39:28 +0200

    add flask project

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request
+from flask_jwt_extended import (JWTManager, jwt_required, create_access_token, get_jwt_identity)
+import os
+from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+
+
+#Import personnalisé
+import model
+
+
+load_dotenv()
+
+app = Flask(__name__)
+
+app.config['JWT_SECRET_KEY'] = 'votre_clé_secrète'  # Remplacez 'votre_clé_secrète' par une clé secrète forte et sécurisée
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
+#db = SQLAlchemy(app)
+jwt = JWTManager(app)
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.json.get('username')
+    password = request.json.get('password')
+
+    # Vérifiez le nom d'utilisateur et le mot de passe (par exemple, dans une base de données)
+    # Si la vérification est réussie, générez un jeton d'accès JWT
+    if username == 'utilisateur' and password == 'motdepasse':
+        access_token = create_access_token(identity=username)
+        return {'access_token': access_token}, 200
+    else:
+        return {'message': 'Authentification échouée'}, 401
+    
+@app.route('/test', methods=['GET'])
+@jwt_required()
+def ressource_protégée():
+    current_user = get_jwt_identity()
+    return {'message': 'Ceci est une ressource protégée', 'user': current_user}
+
+if __name__ == '__main__':
+    app.run(debug=True)
