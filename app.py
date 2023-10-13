21f4fe8ef9db959c184a9ea3a35624b1ff3bb6b3
CyrilGrj <cyril.grosjean@outlook.fr>
2023-10-13 11:10:57 +0200

    jeton jwt auth

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_jwt_extended import (JWTManager, jwt_required, create_access_token, g
 import os
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
+import bcrypt
 
 
 
@@ -20,16 +21,19 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 jwt = JWTManager(app)
 
+
 @app.route('/login', methods=['POST'])
 def login():
-    username = request.json.get('username')
+    identifiant = request.json.get('identifiant')
     password = request.json.get('password')
 
-    # Vérifiez le nom d'utilisateur et le mot de passe (par exemple, dans une base de données)
-    # Si la vérification est réussie, générez un jeton d'accès JWT
-    if username == 'utilisateur' and password == 'motdepasse':
-        access_token = create_access_token(identity=username)
-        return {'access_token': access_token}, 200
+    user = get_user_by_identifiant(identifiant)
+    if (user is not None):
+        # Vérifiez le nom d'utilisateur et le mot de passe (par exemple, dans une base de données)
+        # Si la vérification est réussie, générez un jeton d'accès JWT
+        if (user.check_password(password)):
+            access_token = create_access_token(identity=identifiant)
+            return {'access_token': access_token}, 200
     else:
         return {'message': 'Authentification échouée'}, 401
     
@@ -41,17 +45,35 @@ def ressource_protégée():
 
 
 
+
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    identifiant = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(120), nullable=False)
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+    def __init__(self, identifiant, password):
+        self.identifiant = identifiant
+        self.set_password(password)  # Utilisez la méthode pour définir le mot de passe
+        
+    def set_password(self, password):
+        # Utilisez hashlib pour hacher le mot de passe en MD5
+        salt = bcrypt.gensalt()
+        self.password_hash = bcrypt.hashpw(password.encode('utf-8'), salt)
+    def check_password(self, password):
+        # Vérifiez si le mot de passe fourni correspond au hachage dans la base de données
+        return bcrypt.checkpw(password.encode('utf-8'), self.password_hash.encode('utf-8'))
+
+def get_user_by_identifiant(identifiant):
+    user = User.query.filter_by(identifiant=identifiant).first()
+    return user
 
 with app.app_context():
     db.create_all()
+    # new_user = User(identifiant='nom_utilisateur', password='mot_de_passe')
+    # db.session.add(new_user)
+    # db.session.commit()
     
 if __name__ == '__main__':
     app.run(debug=True)
