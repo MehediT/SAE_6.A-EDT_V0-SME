6b15dbdb958a0eaf9493356e3fdf236b7551fc4b
Cyril Grosjean <cyril.grosjean@algonis.net>
2023-11-16 16:51:18 +0100

    change port

--- database/config.py
+++ database/config.py
@@ -7,6 +7,6 @@ db = SQLAlchemy()
 DB_URI = os.environ.get('DATABASE_URL')
 
 def configure_database(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://courroux:root@db:3306/courroux"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://courroux:root@db:8083/courroux"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
