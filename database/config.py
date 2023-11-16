80eda17936d061afb89397f93d0b3f9bd1eff9dd
Cyril Grosjean <cyril.grosjean@algonis.net>
2023-11-16 16:20:04 +0100

    change database link

--- database/config.py
+++ database/config.py
@@ -7,6 +7,6 @@ db = SQLAlchemy()
 DB_URI = os.environ.get('DATABASE_URL')
 
 def configure_database(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://tiktok_uploader:root@db:3306/tiktok_uploader"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
