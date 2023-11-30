ac732ede01a6fb1238565ffd6e3d9eff2c79fc28
Cyril Grosjean <cyril.grosjean@algonis.net>
2023-11-30 14:30:51 +0100

    finish salle controller

--- routes/__init__.py
+++ routes/__init__.py
@@ -3,11 +3,14 @@ from .auth_routes import auth_bp
 from .api_routes import api_bp
 from .cours_routes import cours_bp
 from .user_routes import user_bp
+from .SalleController import salle_bp
+
 
 def register_routes(app):
     app.register_blueprint(auth_bp)
     app.register_blueprint(api_bp)
     app.register_blueprint(cours_bp)
     app.register_blueprint(user_bp)
+    app.register_blueprint(salle_bp)
 
 # Exportez la fonction pour l'utiliser dans app.py
