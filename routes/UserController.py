d6311dfd059893cad14cf85e1e0d401bf5db0110
Ramirez-Nicolas <nicolas.ramirez2305@hotmail.com>
2023-11-30 15:29:28 +0100

    ajout des routes users

---
+++ routes/UserController.py
@@ -0,0 +1,72 @@
+from flask import Blueprint, jsonify, send_from_directory, request
+from services.UserService import UserService
+from models.User import User
+from functools import wraps
+from flask_jwt_extended import (jwt_required, create_access_token, get_jwt_identity)
+
+user_bp = Blueprint('user', __name__)
+
+@user_bp.route('/user/create', methods=['POST'])
+def create_user():
+    identifier = request.json.get('identifier')
+    password = request.json.get('password')
+    name = request.json.get('name')
+    lastname = request.json.get('lastname')
+
+    try:
+        # Créer un enseignant
+        user = UserService.create_user(identifier, password, name, lastname)
+
+        return jsonify(user.to_dict()),200
+    except Exception as e:
+        # En cas d'erreur, annulez la transaction et renvoyez un message d'erreur
+        # db.session.rollback()
+        return jsonify({'error': str(e)}),403 
+    
+
+@user_bp.route('/users', methods=['GET'])
+def get_all_user():
+    try:
+        users = UserService.get_all_users()
+        users_dict = [user.to_dict() for user in users]
+        return jsonify([users_dict]),200
+    except Exception as e:
+        # En cas d'erreur, annulez la transaction et renvoyez un message d'erreur
+        # db.session.rollback()
+        return jsonify({'error': str(e)}),403
+    
+
+@user_bp.route('/user/<id>', methods=['GET'])
+def get_by_idUser(id):
+    try:
+        user = UserService.get_by_id(id)
+        return jsonify(user.to_dict()),200
+    except Exception as e:
+        # En cas d'erreur, annulez la transaction et renvoyez un message d'erreur
+        # db.session.rollback()
+        return jsonify({'error': str(e)}),403
+    
+
+@user_bp.route('/user/<id>', methods=['DELETE'])
+def delete_user(id):
+    try:
+        user = UserService.get_by_id(id)
+        UserService.delete_user(user)
+        return jsonify(user.to_dict()),200
+    except Exception as e:
+        # En cas d'erreur, annulez la transaction et renvoyez un message d'erreur
+        # db.session.rollback()
+        return jsonify({'error': str(e)}),403
+    
+
+@user_bp.route('/user/<id>', methods=['PUT'])
+def update_user(id):
+    name = request.json.get('name')
+    lastname = request.json.get('lastname')
+    try:
+        user = UserService.update_user(id, name, lastname)
+        return jsonify(user.to_dict()),200
+    except Exception as e:
+        # En cas d'erreur, annulez la transaction et renvoyez un message d'erreur
+        # db.session.rollback()
+        return jsonify({'error': str(e)}),403
