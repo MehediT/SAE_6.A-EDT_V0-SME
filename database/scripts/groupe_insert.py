d62c43481c09dc0af315156ad12b463ab7fdbf25
WeedForSpeedcs <jonas.obrun@gmail.com>
2023-12-06 15:29:20 +0100

    Groupe finish

--- database/scripts/groupe_insert.py
+++ database/scripts/groupe_insert.py
@@ -6,26 +6,26 @@ dataGroupes = [
 
   {
     'niveau' : 3,
-    'name' : 'BUT INFO S3',
+    'name' : 'Groupe B',
     'id_group_parent' : 1
   },
 
   {
     'niveau' : 2,
-    'name' : 'BUT GACO S2',
-    'id_group_parent' : 1
+    'name' : 'Groupe A',
+    'id_group_parent' : 2
   },
 
   {
     'niveau' : 5,
-    'name' : 'BUT QLIO S5',
-    'id_group_parent' : 1
+    'name' : 'Groupe C',
+    'id_group_parent' : 3
   },
 
   {
     'niveau' : 2,
-    'name' : 'BUT GACO S2',
-    'id_group_parent' : 2
+    'name' : 'Groupe A2',
+    'id_group_parent' : 5
   }
 ]
 
