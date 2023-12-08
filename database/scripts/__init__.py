70165850de696eb237eac5d1c50c351cfe10ff69
WeedForSpeedcs <jonas.obrun@gmail.com>
2023-12-08 12:05:53 +0100

    Resolve absence

--- database/scripts/__init__.py
+++ database/scripts/__init__.py
@@ -10,7 +10,8 @@ import database.scripts.teacher_insert
 import database.scripts.ressources_insert
 import database.scripts.disponibilite_insert
 import database.scripts.user_groupe_insert
-#import database.scripts.cours_insert
-#import database.scripts.absence_insert
+import database.scripts.cours_insert
+import database.scripts.absence_insert
+import database.scripts.studentAbsencesCours_insert
 
 
--- database/scripts/studentAbsencesCours_insert.py
+++ database/scripts/studentAbsencesCours_insert.py
@@ -31,3 +31,10 @@ dataStudentAbsencesCours = [
         'id_cours' : 3
     }
 ]
+
+
+for student_absences_cours in dataStudentAbsencesCours:
+
+    idStudent = student_absences_cours["id_student"]
+    idCours = student_absences_cours["id_cours"]
+    StudentAbsencesCoursService.add_absence_to_cours(idStudent,idCours)
