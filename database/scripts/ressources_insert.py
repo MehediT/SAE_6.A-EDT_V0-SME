ad2591d1f476cd9a68b3b4e1921a63dffd1ce9bd
cgrosjean <101260907+CyrilGrj@users.noreply.github.com>
2023-12-21 10:23:40 +0100

    resolve script sql

--- database/scripts/ressources_insert.py
+++ database/scripts/ressources_insert.py
@@ -8,19 +8,22 @@ dataRessources = [
   {
     'initial' : 'R5A08',
     'name' : 'Qualite de dev',
-    'id_promo' : 2
+    'id_promo' : 2,
+    'color':'0D3978'
   },
 
   {
     'initial' : 'R5A12',
     'name' : 'Modelisations',
-    'id_promo' : 1
+    'id_promo' : 1,
+    'color':'590D78'
   },
 
   {
     'initial' : 'R5A05',
     'name' : 'PPP',
-    'id_promo' : 3
+    'id_promo' : 3,
+    'color':'78650D'
   }
 ]
 
