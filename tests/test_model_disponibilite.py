b7f3cea61a4298a1bd6599d21fd51587cbacba00
WeedForSpeedcs <jonas.obrun@gmail.com>
2023-12-21 12:15:14 +0100

    ajout test model disponibilite

---
+++ tests/test_model_disponibilite.py
@@ -0,0 +1,58 @@
+import pytest
+from database.config import db
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from models.Disponibilite import Disponibilite  
+from datetime import datetime
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    return app
+
+@pytest.fixture(name="app")
+def app():
+    app = create_app()
+
+    with app.app_context():
+        db.create_all()
+
+    yield app
+
+    with app.app_context():
+        db.drop_all()
+
+def test_disponibilite_initialization(app):
+    # Test the initialization of the Disponibilite class
+
+    # Create a test instance
+    disponibilite = Disponibilite(
+        enseignant="JM",
+        disponible=True,
+        date_debut_disponibilite= datetime(2023,1,1,8,00,00),
+        date_fin_disponibilite= datetime(2023,1,1,17,00,00)
+    )
+
+    # Add the instance to the test database
+    with app.app_context():
+        db.session.add(disponibilite)
+        db.session.commit()
+
+    # Retrieve the instance from the database
+    with app.app_context():
+        retrieved_disponibilite = Disponibilite.query.filter_by(enseignant="JM").first()
+
+    # Perform assertions to check if the instance was added and retrieved correctly
+    assert retrieved_disponibilite is not None
+    assert retrieved_disponibilite.disponible is True
+    assert str(retrieved_disponibilite.date_debut_disponibilite) == "2023-01-01 08:00:00"
+    assert str(retrieved_disponibilite.date_fin_disponibilite) == "2023-01-01 17:00:00"
+
+    # Clean up the database (optional)
+    with app.app_context():
+        db.session.delete(retrieved_disponibilite)
+        db.session.commit()
