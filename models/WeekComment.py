4e4fc0a7c839e832489e0170dcdb65400bea9377
cgrosjean <101260907+CyrilGrj@users.noreply.github.com>
2023-12-24 00:03:11 +0100

    resolve

--- models/WeekComment.py
+++ models/WeekComment.py
@@ -5,21 +5,24 @@ class WeekComment(db.Model):
     __tablename__ = "week_comment"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id_promo = db.Column(db.Integer, db.ForeignKey('promo.id'), nullable=False)
     week_number = db.Column(db.Integer, nullable=False)
     year = db.Column(db.Integer, nullable=False)
     content = db.Column(db.String(255), nullable=False)
 
-    def __init__(self, week_number, year, content):
+    def __init__(self, week_number, year, content, id_promo):
         self.week_number = week_number
         self.year = year
         self.content = content
+        self.id_promo = id_promo
 
     def to_dict(self):
         return {
             'id': self.id,
             'week_number': self.week_number,
             'year': self.year,
-            'content': self.content
+            'content': self.content,
+            'id_promo': self.id_promo
         }
         
   
