599d9b9460e62044fdc242e9c61de5a3cba89426
cgrosjean <101260907+CyrilGrj@users.noreply.github.com>
2024-01-17 18:53:17 +0100

    resolve

--- services/UserGroupeService.py
+++ services/UserGroupeService.py
@@ -39,6 +39,7 @@ class UserGroupeService:
         groupes = [id_group for id_student, id_group in result]
         groupes_tree_list = []
         for groupe in groupes:
+            groupes_tree_list.append(groupe)
             #Add the groupe id to the list no duplicates
             for groupe_tree in GroupeService.get_parents_list(groupe):
                 if groupe_tree not in groupes_tree_list:
