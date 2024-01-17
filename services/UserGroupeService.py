052e378eea01c10c47766d72a098786284e72e3a
cgrosjean <101260907+CyrilGrj@users.noreply.github.com>
2024-01-17 18:41:53 +0100

    add group tree for course

--- services/UserGroupeService.py
+++ services/UserGroupeService.py
@@ -33,8 +33,14 @@ class UserGroupeService:
 
         result = query.all()
         groupes = [id_group for id_student, id_group in result]
+        groupes_tree_list = []
+        for groupe in groupes:
+            #Ajouter dans liste de groupe tree sans doublons
+            for groupe_tree in GroupeService.get_tree(groupe):
+                if groupe_tree not in groupes_tree_list:
+                    groupes_tree_list.append(groupe_tree)
 
-        return groupes
+        return groupes_tree_list
 
     @staticmethod
     def get_etudiant_by_groupe(idGroupe, idStudent):
