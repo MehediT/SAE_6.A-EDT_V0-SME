7355664edf091f3796496ef5744d938c8ea0a212
Ramirez-Nicolas <nicolas.ramirez2305@hotmail.com>
2024-01-22 09:12:20 +0100

    ajout commentaire Model Absence

--- models/Absence.py
+++ models/Absence.py
@@ -2,14 +2,19 @@ from database.config import db
 from models.Cours import Cours
 from models.Student import Student
 
+# Le modèle Absence représente une absence d'un étudiant à un cours spécifique
 
 class Absence(db.Model):
     __tablename__= "absence"
 
+    # Clé primaire composée : ID de l'étudiant et ID du cours
     idEtudiant = db.Column(db.BigInteger, db.ForeignKey('student.id_student'), primary_key=True)
     idCour = db.Column(db.Integer, db.ForeignKey('cours.id'), primary_key=True)
+    
+    # Champ indiquant si l'absence est justifiée (True) ou non (False)
     justifiée = db.Column(db.Boolean)
 
+    # Constructeur de la classe Absence
     def __init__(self, idEtudiant, idCour):
         self.idEtudiant = idEtudiant
         self.idCour = idCour
