763d2dfe99d81a4fb183865162ed8322d960c98d
ismaelargence <ismaelargence3@gmail.com>
2024-03-06 01:24:17 +0100

    refix

--- database/scripts/cours_insert.py
+++ database/scripts/cours_insert.py
@@ -8,7 +8,126 @@ from datetime import date, time
 date
 
 dataCours = [
-    
+    {
+      "id": 1,
+      "start_time": "2024-03-04 08:00:00",
+      "end_time": "2024-03-04 10:00:00",
+      "id_enseignant": 1,
+      "initial_ressource": "R3-01",
+      "id_group": 1,
+      "name_salle": "B1-09",
+      "appelEffectue": 0,
+      "is_published": 1,
+      "evaluation": 0
+    },
+    {
+      "id": 2,
+      "start_time": "2024-03-04 10:00:00",
+      "end_time": "2024-03-04 11:00:00",
+      "id_enseignant": 2,
+      "initial_ressource": "R3-02",
+      "id_group": 1,
+      "name_salle": "B1-10",
+      "appelEffectue": 0,
+      "is_published": 1,
+      "evaluation": 0
+    },
+    {
+      "id": 3,
+      "start_time": "2024-03-04 12:00:00",
+      "end_time": "2024-03-04 14:00:00",
+      "id_enseignant": 1,
+      "initial_ressource": "R3-03",
+      "id_group": 1,
+      "name_salle": "B1-11",
+      "appelEffectue": 0,
+      "is_published": 1,
+      "evaluation": 0
+    },
+    {
+      "id": 4,
+      "start_time": "2024-03-04 14:30:00",
+      "end_time": "2024-03-04 16:00:00",
+      "id_enseignant": 2,
+      "initial_ressource": "R3-04",
+      "id_group": 1,
+      "name_salle": "B1-12",
+      "appelEffectue": 0,
+      "is_published": 1,
+      "evaluation": 0
+    },
+    {
+      "id": 5,
+      "start_time": "2024-03-04 16:00:00",
+      "end_time": "2024-03-04 17:30:00",
+      "id_enseignant": 1,
+      "initial_ressource": "R3-05",
+      "id_group": 1,
+      "name_salle": "B1-13",
+      "appelEffectue": 0,
+      "is_published": 1,
+      "evaluation": 0
+    },
+    {
+      "id": 6,
+      "start_time": "2024-03-05 13:00:00",
+      "end_time": "2024-03-05 15:00:00",
+      "id_enseignant": 2,
+      "initial_ressource": "R3-06",
+      "id_group": 1,
+      "name_salle": "B1-14",
+      "appelEffectue": 0,
+      "is_published": 1,
+      "evaluation": 0
+    },
+    {
+      "id": 7,
+      "start_time": "2024-03-05 10:00:00",
+      "end_time": "2024-03-05 12:00:00",
+      "id_enseignant": 1,
+      "initial_ressource": "R3-07",
+      "id_group": 1,
+      "name_salle": "B1-15",
+      "appelEffectue": 0,
+      "is_published": 1,
+      "evaluation": 0
+    },
+    {
+      "id": 8,
+      "start_time": "2024-03-05 15:00:00",
+      "end_time": "2024-03-05 17:00:00",
+      "id_enseignant": 2,
+      "initial_ressource": "R3-08",
+      "id_group": 1,
+      "name_salle": "B1-16",
+      "appelEffectue": 0,
+      "is_published": 1,
+      "evaluation": 0
+    },
+    {
+      "id": 9,
+      "start_time": "2024-03-05 08:00:00",
+      "end_time": "2024-03-05 10:00:00",
+      "id_enseignant": 1,
+      "initial_ressource": "R3-09",
+      "id_group": 1,
+      "name_salle": "B1-17",
+      "appelEffectue": 0,
+      "is_published": 1,
+      "evaluation": 0
+    },
+    {
+      "id": 10,
+      "start_time": "2024-03-06 10:00:00",
+      "end_time": "2024-03-06 12:00:00",
+      "id_enseignant": 2,
+      "initial_ressource": "R3-10",
+      "id_group": 1,
+      "name_salle": "B1-18",
+      "appelEffectue": 0,
+      "is_published": 1,
+      "evaluation": 0
+    }
 ]
 
 
--- database/scripts/salle_insert.py
+++ database/scripts/salle_insert.py
@@ -4,7 +4,68 @@ from services.SalleService import SalleService
 
 
 
-dataSalles = []
+dataSalles = [
+    {
+        'name': 'B1-09',
+        'ordi' : 30,
+        'tableauNumerique' : 1,
+        'videoProjecteur' : 1
+    },
+    {
+        'name': 'B1-10',
+        'ordi' : 30,
+        'tableauNumerique' : 1,
+        'videoProjecteur' : 1
+    },
+    {
+        'name': 'B1-11',
+        'ordi' : 30,
+        'tableauNumerique' : 1,
+        'videoProjecteur' : 1
+    },
+    {
+        'name': 'B1-12',
+        'ordi' : 30,
+        'tableauNumerique' : 1,
+        'videoProjecteur' : 1
+    },
+    {
+        'name': 'B1-13',
+        'ordi' : 30,
+        'tableauNumerique' : 1,
+        'videoProjecteur' : 1
+    },
+    {
+        'name': 'B1-14',
+        'ordi' : 30,
+        'tableauNumerique' : 1,
+        'videoProjecteur' : 1
+    },
+    {
+        'name': 'B1-15',
+        'ordi' : 30,
+        'tableauNumerique' : 1,
+        'videoProjecteur' : 1
+    },
+    {
+        'name': 'B1-16',
+        'ordi' : 30,
+        'tableauNumerique' : 1,
+        'videoProjecteur' : 1
+    },
+    {
+        'name': 'B1-17',
+        'ordi' : 30,
+        'tableauNumerique' : 1,
+        'videoProjecteur' : 1
+    },
+    {
+        'name': 'B1-18',
+        'ordi' : 30,
+        'tableauNumerique' : 1,
+        'videoProjecteur' : 1
+    },
+]
 
 
 for salles in dataSalles:
